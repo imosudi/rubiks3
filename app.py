2da0b381a442d7373e0a49eee270b43d4acfa70d
Mosudi I. O <imosudi@gmail.com>
2019-11-12 12:24:06 +0100

    1st app iteration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_script import Manager
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
@@ -31,8 +31,31 @@ def index():
     formM = forms.rubikFormM()
     rubikOp = logics.rubikOperation()
     num = rubikOp.inputNbyNbyN()
-    print(num)
+    print(type(num))
+    if num is None:
+        flash('')
+        num = 3
+        cubeletSum = 27 
+        FacesCubelets = 26
+        hidden = 1
+        m = 1
+
+    elif num > 1 and num < 15:
+        cubeletSum = rubikOp.numCubelets()
+        FacesCubelets = rubikOp.numFacesCubelets()
+        hidden = rubikOp.hiddenCubelets()
+        m = rubikOp.nM()
+        formM.m.data  = m
+    elif num > 14:
+        flash ('Provide n (1<n<14) TOO MUCH')
+        num = 3
+        cubeletSum = 27 
+        FacesCubelets = 26 
+        hidden = 1 
+        m = 1 
+        formM.m.data  = m
     return render_template('index.html',
+    FacesCubelets=FacesCubelets, hidden=hidden, m=m, cubeletSum=cubeletSum, 
     form=form, formM=formM, num=num, current_time=datetime.utcnow())
     pass
 
@@ -53,3 +76,8 @@ if __name__ == "__main__":
 """if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
     #manager.run()"""
+
+    
+
+
+
