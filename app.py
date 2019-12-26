fec506d0f3668433fcfb7876e95b8a1c2504dd3d
Mosudi I. O <imosudi@gmail.com>
2019-12-26 19:46:22 +0100

    Modify logfile name

--- app.py
+++ app.py
@@ -47,7 +47,10 @@ moment = Moment(app)
 
 #Creating log filename
 current_time=datetime.now()
-logfilename = str(current_time) + '.log'
+logfilename = str(current_time)
+logfilename = logfilename.replace(" ", "_")
+logfilename = logfilename.replace(":", "_")
+logfilename = logfilename.replace(".", "_") + '.log'
 logfile = os.path.join('log_dir/', logfilename)
 logging.basicConfig(filename=logfile, level=logging.INFO, 
         format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
