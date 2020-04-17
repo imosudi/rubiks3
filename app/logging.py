91b12a11a2850efc7690879f5cc6aced2cff26d0
imosudi <imosudi@gmail.com>
2020-04-17 18:26:37 +0100

    App configuration

--- app/logging.py
+++ app/logging.py
@@ -7,9 +7,9 @@ logfilename = str(current_time)
 logfilename = logfilename.replace(" ", "_")
 logfilename = logfilename.replace(":", "_")
 logfilename = logfilename.replace(".", "_") + '.log'
-logfile = os.path.join('log_dir/', logfilename)
-logging.basicConfig(filename=logfile, level=logging.DEBUG, 
-        format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+#logfile = os.path.join('log_dir/', logfilename)
+#logging.basicConfig(filename=logfile, level=logging.DEBUG, 
+ #       format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 #logging.basicConfig(filename=logfile, level=logging.DEBUG)
 #printout()
