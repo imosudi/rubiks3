6e86e8c180a5e12f84ae848e745d971447cf4303
imosudi <imosudi@gmail.com>
2020-04-17 18:56:47 +0100

    App configuration

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,24 @@ from flask_script import Manager
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+
 from app import views
 from app import config
 from app import logics
 from app import forms
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#from datetime import datetime
+import timeit
+import time
+import io
+import base64
+
+app = Flask(__name__)
 #from app import logging
 
 from app.cube2 import Cube
