c69e9dde52a9c85619c89598e05626a9df79053c
imosudi <imosudi@gmail.com>
2020-04-17 16:51:56 +0000

    App configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, flash
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 from app import views
+from app import config
+from app import logics
+from app import forms
+from app import logging
+
+from app.cube2 import Cube
+
+
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+manager = Manager(app)
+moment = Moment(app)
+
---
+++ app/config.py
@@ -0,0 +1,35 @@
+#config.py
+import os
+import sys
+#sys.path.insert(0, "../venv/lib/python3.6/site-packages")
+#Dockerfile COPY --from=builder /app/build "../venv/lib/python3.6/site-packages"
+sys.path.insert(0, "../python3.6/site-packages")
+
+from os import environ as env
+#Importing Logging
+import logging
+import os.path
+#import multiprocessing
+
+
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+import timeit
+import time
+import io
+import base64
+
+
+
+PORT = int(env.get("PORT", 9082))
+DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
+
+#Gunicorn config
+bind = ":" + str(PORT)
+#workers = multiprocessing.cpu_count() * 2 + 1
+#threads = 2 * multiprocessing.cpu_count()
