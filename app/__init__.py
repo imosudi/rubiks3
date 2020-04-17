5fb9581ebd571b2bb216931b01a4655f98d2a57b
imosudi <imosudi@gmail.com>
2020-04-17 19:01:32 +0100

    App configuration

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from flask_bootstrap import Bootstrap
 
 from app import views
 #from app import config
-from app import logics
-from app import forms
+#from app import logics
+#from app import forms
 
 import numpy as np
 import matplotlib
