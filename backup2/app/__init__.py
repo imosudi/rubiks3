5aa9f4384de531209183f03c595073754b8240b6
imosudi <imosudi@gmail.com>
2020-04-20 03:15:28 +0000

    App configuration

---
+++ backup2/app/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, flash
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+
+
+from app import views
+#from app import config
+#from app import logics
+#from app import forms
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#from datetime import datetime
+import timeit
+import time
+import io
+import base64
+
+app = Flask(__name__)
+#from app import logging
+
+from app.cube2 import Cube
+
+PORT = int(env.get("PORT", 9082))
+DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
+
+#Gunicorn config
+bind = ":" + str(PORT)
+
+app['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+manager = Manager(app)
+moment = Moment(app)
+
