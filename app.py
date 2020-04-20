87a1c14e874db2237b96eb615558085c0fcb2010
imosudi <imosudi@gmail.com>
2020-04-20 17:19:01 +0100

    App configuration

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-import os
-import sys
+from flask import Flask
 
-from flask import Flask, render_template, flash
-"""from flask_script import Manager
-from flask_moment import Moment
-from flask_bootstrap import Bootstrap"""
 
 
 app = Flask(__name__)
 
 
 
-
-"""app['SECRET_KEY'] = 'hard to guess string'"""
-
-"""bootstrap = Bootstrap(app)
-
-manager = Manager(app)
-moment = Moment(app)"""
-
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
