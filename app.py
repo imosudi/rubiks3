f58930167a9f888482d8e425b2e988cee9593f2d
imosudi <imosudi@gmail.com>
2020-04-20 10:27:06 +0100

    App configuration

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import os
 import sys
 
+from flask import Flask, render_template, flash
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+
+
+
+
+app['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+manager = Manager(app)
+moment = Moment(app)
+
 
 
 if __name__ == "__main__":
