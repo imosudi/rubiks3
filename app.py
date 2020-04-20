c0dd02f59c83f5d739a564abd506f0c330d7dabc
imosudi <imosudi@gmail.com>
2020-04-20 10:14:17 +0100

    App configuration

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, flash
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+
+
+import views
+import config
+import logics
+import forms
+
+"""import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt"""
+
+
+app = Flask(__name__)
+
+
+#from app.cube2 import Cube
+
+"""PORT = int(env.get("PORT", 9082))
+DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
+
+#Gunicorn config
+bind = ":" + str(PORT)"""
+
+app['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+
+manager = Manager(app)
+moment = Moment(app)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
---
+++ logging.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+#Creating log filename
+"""current_time=datetime.now()
+logfilename = str(current_time)
+logfilename = logfilename.replace(" ", "_")
+logfilename = logfilename.replace(":", "_")
+logfilename = logfilename.replace(".", "_") + '.log'"""
+#logfile = os.path.join('log_dir/', logfilename)
+#logging.basicConfig(filename=logfile, level=logging.DEBUG, 
+ #       format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+#logging.basicConfig(filename=logfile, level=logging.DEBUG)
+#printout()
+
