47f934e3ce055ddff56191cc8497948531896518
imosudi <imosudi@gmail.com>
2020-04-20 10:09:10 +0100

    App configuration

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,21 @@ from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 
 
-from app import views
-from app import config
-from app import logics
-from app import forms
+import views
+import config
+import logics
+import forms
 
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-#from datetime import datetime
-"""import timeit
-import time
-import io
-import base64"""
 
 app = Flask(__name__)
-#from app import logging
 
-from app.cube2 import Cube
+
+#from app.cube2 import Cube
 
 PORT = int(env.get("PORT", 9082))
 DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
