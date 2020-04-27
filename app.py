9757290e2fdb3e03b50c7c6955ad29ea7a4e387c
imosudi <imosudi@gmail.com>
2020-04-27 17:00:40 +0100

    App configuration

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
 
-import numpy as np
+"""import numpy as np
 #import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
 from datetime import datetime
-import time
+import time"""
 
 
 
