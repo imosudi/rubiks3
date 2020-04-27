433abae51bf1b98019dd6a7713733a8bb1d5eed3
imosudi <imosudi@gmail.com>
2020-04-27 16:12:23 +0100

    App configuration

--- views.py
+++ views.py
@@ -12,13 +12,13 @@ def another():
     pageName = "another"
     return render_template('about.html', pageName = pageName,  current_time=datetime.utcnow())
 
+@app.route('/contact', methods = ['GET'] )
+def contact():
+    pageName = "contact"
+    return render_template('contact.html',  pageName = pageName, current_time=datetime.utcnow())
 
 @app.route('/about', methods = ['GET'])
 def about():
     pageName = "about"
     return render_template('about.html', pageName = pageName,  current_time=datetime.utcnow())
 
-@app.route('/contact', methods = ['GET'] )
-def contact():
-    pageName = "contact"
-    return render_template('contact.html',  pageName = pageName, current_time=datetime.utcnow())
