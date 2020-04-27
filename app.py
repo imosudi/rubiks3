bb28b7520ca7489f7840a825632f139a3c0493c5
imosudi <imosudi@gmail.com>
2020-04-27 14:15:02 +0100

    App configuration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 
+import config
+
 
 app = Flask(__name__)
 
@@ -12,4 +14,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
