5457fa0d2c84762da0556afb5a7bcf52f84ad5a9
imosudi <imosudi@gmail.com>
2020-04-27 15:56:38 +0100

    App configuration

---
+++ views.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+from datetime import datetime
+import time
+
+
+from app import app
+
+@app.route('/another', methods = ['GET'])
+def another():
+    pageName = "about"
+    return render_template('about.html', pageName = pageName,  current_time=datetime.utcnow())
