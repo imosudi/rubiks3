9fb8e28bf9d8c47445fc46888cd171d4de8317f9
imosudi <imosudi@gmail.com>
2020-04-27 14:25:31 +0100

    App configuration

--- config.py
+++ config.py
@@ -1,24 +1,9 @@
+
 #config.py
 import os
 import sys
 from os import environ as env
-
-env = os.environ.get('FLASK_ENV', environment)
-
-#sys.path.insert(0, "../venv/lib/python3.6/site-packages")
-#Dockerfile COPY --from=builder /app/build "../venv/lib/python3.6/site-packages"
-#sys.path.insert(0, "../python3.6/site-packages")
-
-#from os import environ as env
-#Importing Logging
-#import logging
-#import os.path
 #import multiprocessing
-
-
-
-
-
 
 PORT = int(env.get("PORT", 9082))
 DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
