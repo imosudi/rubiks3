3bdbf484545a11f7b857891393f5e7f6d897c205
imosudi <imosudi@gmail.com>
2020-04-27 16:16:06 +0100

    App configuration

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+
+from datetime import datetime
+import time
+
+
+from app import app
+
+@app.route('/another', methods = ['GET'])
+def another():
+    pageName = "another"
+    return render_template('about.html', pageName = pageName,  current_time=datetime.utcnow())
+
+@app.route('/contact', methods = ['GET'] )
+def contact():
+    pageName = "contact"
+    return render_template('contact.html',  pageName = pageName, current_time=datetime.utcnow())
+
+@app.route('/about', methods = ['GET'])
+def about():
+    pageName = "about"
+    return render_template('about.html', pageName = pageName,  current_time=datetime.utcnow())
+
